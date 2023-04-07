cf14547c056d6f66afb4a98511028f9884c5700b
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-04-07 22:05:29 +0000

    Create a model and post data to api

---
+++ models/productModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, 'Kindly enter a product name']
+        },
+        quantity: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        price: {
+            type: Number,
+            required: true,
+        },
+        image: {
+            type: String,
+            required: false
+        } 
+    },
+    {
+        timestamps: true
+    }
+)
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const Product = require('./models/productModel')
 const app = express()
+
+
+app.use(express.json())
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Node API')
+    res.send('Node API is here')
 })
 
-app.listen(3000, () => {
-    console.log('Node app is running on port 3000')
+app.post('/product', async (req, res) => {
+    try {
+        const product = await Product.create(req.body)
+        res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+mongoose.connect('mongodb+srv://admin:Jeremiah123@jayapi.rkgchnw.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000, () => {
+        console.log('Node app is running on port 3000')
+    })
+    console.log('Connected to mongoDB')
+}).catch((error) => {
+    console.log('error')
 })
