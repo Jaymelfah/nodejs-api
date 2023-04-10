af41e446a4ed130b4b40c59fb0f4d4be9a17beaa
Jeremiah Melfah <jeremiahmelfah@Jeremiahs-MacBook-Air.local>
2023-04-10 06:32:54 +0000

    Add update and delete api endpoints

--- server.js
+++ server.js
@@ -5,11 +5,65 @@ const app = express()
 
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false})) // if you want to send the data with a form 
 
 // routes
 app.get('/', (req, res) => {
     res.send('Node API is here')
+}) 
+
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
+
+app.get('/products/:id', async(req, res) =>  {
+    try {
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// update product
+app.put('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        const updatedProduct = await Product.findById(id); 
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+// delete a product
+app.delete('/products/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: `cannot find any product with ID ${id}`})
+        }
+        res.status(200).json(product);
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+
+})
+
 
 app.post('/product', async (req, res) => {
     try {
